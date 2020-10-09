79c52b9e032d5966304e8584bcc07dec3fa8cfb6
sora1441 <43249102+sora1441@users.noreply.github.com>
2020-10-09 07:46:13 -0500

    Update main.py

--- Assignments/A01/11727/main.py
+++ Assignments/A01/11727/main.py
@@ -1,8 +1,19 @@
-if __name__ == "__main__":
+def main():
     K = int(input())
     for i in range(K):
-       a = int(input())
-       b = int(input()) 
-       c = int(input())
-       d = max(a,b,c)
-       print("Case ", i+1,": ", d)
+      a, b, c = map (int, input().split())
+      if(a<b<c):
+        e=b
+      elif(a<c<b):
+        e=c
+      elif(b<a<c):
+        e=a
+      elif(b<c<a):
+        e=c
+      elif(c<a<b):
+        e=a
+      elif(c<b<a):
+        e=b
+      print("Case ", i+1,": ", e, sep='')
+if __name__ == "__main__":
+    main()
