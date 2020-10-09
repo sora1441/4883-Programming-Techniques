27bff9307d5eb941e2f6114003227f20d42137a2
sora1441 <43249102+sora1441@users.noreply.github.com>
2020-10-09 08:09:16 -0500

    Update main.py

--- Assignments/A01/12372/main.py
+++ Assignments/A01/12372/main.py
@@ -1,10 +1,10 @@
-if __name__ == "__main__":
+def main():
     K = int(input())
     for i in range(K):
-        l = int(input())
-        w = int(input())
-        h = int(input())
+        l, w, h = map(int,input().split())
         if l > 20 or w > 20 or h > 20:
-            print("Case ", i+1,": bad")
+            print("Case ", i+1,": bad",sep='')
         else:
-            print("Case ", i+1,": good")
+            print("Case ", i+1,": good",sep='')
+if __name__ == "__main__":
+  main()
